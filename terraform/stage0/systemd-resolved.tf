b4a9712f3a97ac2191a783235f3988117679552e
Ray Saltrelli <ray@saltrelli.com>
2023-08-20 19:47:37 -0400

    Remove boot parameters

--- terraform/stage0/systemd-resolved.tf
+++ terraform/stage0/systemd-resolved.tf
@@ -14,6 +14,4 @@ resource "null_resource" "systemd_resolved" {
       "sudo sed -i 's/nameserver 127.0.0.53/nameserver 208.67.222.222\\nnameserver 208.67.220.220/g' /etc/resolv.conf"
     ]
   }
-
-  depends_on = [null_resource.boot_parameters]
 }
