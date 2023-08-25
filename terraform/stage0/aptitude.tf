6db5d85f94640b121d687c01f02441d6413fec79
Ray Saltrelli <ray@saltrelli.com>
2023-08-25 14:32:32 -0400

    Don't install Linux modules for Raspberry Pi

--- terraform/stage0/aptitude.tf
+++ terraform/stage0/aptitude.tf
@@ -1,24 +1,3 @@
-resource "null_resource" "linux_modules" {
-  connection {
-    host = var.host
-    user = var.user
-  }
-
-  provisioner "remote-exec" {
-    when = create
-    inline = [
-      "echo ${var.password} | sudo -p '' -S true",
-      "sudo apt -y remove needrestart",
-      "sudo apt install -y linux-modules-extra-raspi",
-      "if [ -f /var/run/reboot-required ]; then sudo reboot; fi"
-    ]
-
-    on_failure = continue
-  }
-
-  depends_on = [null_resource.systemd_resolved]
-}
-
 resource "null_resource" "upgrade_packages" {
   connection {
     host = var.host
@@ -35,5 +14,5 @@ resource "null_resource" "upgrade_packages" {
     ]
   }
 
-  depends_on = [null_resource.linux_modules]
+  depends_on = [null_resource.systemd_resolved]
 }
